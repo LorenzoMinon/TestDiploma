7e8567d0ed4806c9331f53fcecd5c13f65688eb8
Lorenzo Miñón <lorenzominon01@gmail.com>
2024-07-28 18:57:57 -0300

    Detalles esteticos form gestion de permisos

--- CapaPresentacion/frmPermisosSimples.Designer.cs
+++ CapaPresentacion/frmPermisosSimples.Designer.cs
@@ -29,6 +29,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPermisos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvGruposPermisos)).BeginInit();
             this.SuspendLayout();
@@ -36,7 +37,7 @@
             // cbUsuarios
             // 
             this.cbUsuarios.FormattingEnabled = true;
-            this.cbUsuarios.Location = new System.Drawing.Point(267, 57);
+            this.cbUsuarios.Location = new System.Drawing.Point(309, 72);
             this.cbUsuarios.Name = "cbUsuarios";
             this.cbUsuarios.Size = new System.Drawing.Size(244, 21);
             this.cbUsuarios.TabIndex = 0;
@@ -48,7 +49,7 @@
             this.dgvPermisos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colNombrePermiso,
             this.colAsignadoPermiso});
-            this.dgvPermisos.Location = new System.Drawing.Point(17, 124);
+            this.dgvPermisos.Location = new System.Drawing.Point(44, 139);
             this.dgvPermisos.Name = "dgvPermisos";
             this.dgvPermisos.Size = new System.Drawing.Size(244, 376);
             this.dgvPermisos.TabIndex = 1;
@@ -70,7 +71,7 @@
             this.dgvGruposPermisos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colNombreGrupoPermiso,
             this.colAsignadoGrupoPermiso});
-            this.dgvGruposPermisos.Location = new System.Drawing.Point(517, 124);
+            this.dgvGruposPermisos.Location = new System.Drawing.Point(571, 139);
             this.dgvGruposPermisos.Name = "dgvGruposPermisos";
             this.dgvGruposPermisos.Size = new System.Drawing.Size(243, 376);
             this.dgvGruposPermisos.TabIndex = 2;
@@ -88,7 +89,7 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(335, 457);
+            this.btnGuardar.Location = new System.Drawing.Point(385, 472);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(98, 43);
             this.btnGuardar.TabIndex = 3;
@@ -101,7 +102,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(190, 25);
             this.label1.TabIndex = 4;
@@ -112,7 +113,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(316, 34);
+            this.label2.Location = new System.Drawing.Point(355, 49);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 20);
             this.label2.TabIndex = 5;
@@ -123,7 +124,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(55, 101);
+            this.label3.Location = new System.Drawing.Point(85, 116);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(147, 20);
             this.label3.TabIndex = 6;
@@ -134,15 +135,26 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(559, 96);
+            this.label4.Location = new System.Drawing.Point(601, 116);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(170, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Grupos de permisos";
             // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.White;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 24);
+            this.label10.Name = "label10";
+            this.label10.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
+            this.label10.Size = new System.Drawing.Size(860, 522);
+            this.label10.TabIndex = 97;
+            // 
             // frmPermisosSimples
             // 
-            this.ClientSize = new System.Drawing.Size(869, 527);
+            this.ClientSize = new System.Drawing.Size(922, 555);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -151,6 +163,7 @@
             this.Controls.Add(this.dgvGruposPermisos);
             this.Controls.Add(this.dgvPermisos);
             this.Controls.Add(this.cbUsuarios);
+            this.Controls.Add(this.label10);
             this.Name = "frmPermisosSimples";
             this.Load += new System.EventHandler(this.frmPermisosSimples_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPermisos)).EndInit();
@@ -174,5 +187,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
     }
 }
